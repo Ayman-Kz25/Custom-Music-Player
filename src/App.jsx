e9048e1412eb93135d803619bfe3990b6a0ed2b6
Ayman-Kz25 <kamalakz786@gmail.com>
2026-02-18 08:45:51 +0500

    forth commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,64 +7,82 @@ import {
   SkipBack,
   SkipForward,
 } from "lucide-react";
-import React from "react";
+
+import lily from "./assets/img/lily.jpg";
 
 function App() {
   return (
-    <div className="main-container">
-      <div className="wrapper">
-        <div className="top-bar">
-          <ChevronDown size={24} strokeWidth={1.5} className="icon" />
-          <span>Now Playing</span>
-          <Ellipsis size={24} strokeWidth={1.5} className="icon" />
-        </div>
-        <div className="img-area">
-          <img src="./assets/img/lily.jpg" alt="" />
-        </div>
-        <div className="song-details">
-          <p className="name">Lily</p>
-          <p className="artist">Alan Walker & K-391 ft. Emelie Hollow</p>
-        </div>
-        <div className="progress-area">
-          <div className="progress-bar">
-            <span></span>
+    <>
+      <svg width="0" height="0">
+        <defs>
+          <linearGradient id="iconGradient" x1="0%" y1="0%" x2="100%" y2="100%">
+            <stop offset="0%" stopColor="#FF74A4" />
+            <stop offset="100%" stopColor="#9F6EA3" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <div className="main-container">
+        <div className="wrapper">
+          <div className="top-bar">
+            <ChevronDown size={24} strokeWidth={1.5} className="icon" />
+            <span>Now Playing</span>
+            <Ellipsis size={24} strokeWidth={1.5} className="icon" />
+          </div>
+          <div className="img-area">
+            <img src={lily} alt="Lily" />
+          </div>
+          <div className="song-details">
+            <p className="name">Lily - Different World</p>
+            <p className="artist">Alan Walker & K-391 ft. Emelie Hollow</p>
+          </div>
+          <div className="progress-area">
+            <div className="progress-bar"></div>
             <div className="timer">
               <span className="current">0:20</span>
               <span className="current">3:10</span>
             </div>
-            <div className="controls">
-              <Repeat
-                size={20}
+          </div>
+          <div className="controls">
+            <Repeat
+              size={24}
+              strokeWidth={1.5}
+              id="repeat"
+              className="icon"
+              style={{ stroke: "url(#iconGradient)" }}
+            />
+            <SkipBack
+              size={28}
+              strokeWidth={1.5}
+              id="prev"
+              className="icon"
+              style={{ stroke: "url(#iconGradient)" }}
+            />
+            <div className="play-pause">
+              <Play
+                size={24}
                 strokeWidth={1.5}
-                id="repeat"
                 className="icon"
-              />
-              <SkipBack
-                size={20}
-                strokeWidth={1.5}
-                id="prev"
-                className="icon"
-              />
-              <div className="play-pause">
-                <Play size={20} strokeWidth={1.5} className="icon" />
-              </div>
-              <SkipForward
-                size={20}
-                strokeWidth={1.5}
-                id="next"
-                className="icon"
-              />
-              <ListMusic
-                size={20}
-                strokeWidth={1.5}
-                id="more-music"
-                className="icon"
+                style={{ stroke: "url(#iconGradient)" }}
               />
             </div>
+            <SkipForward
+              size={28}
+              strokeWidth={1.5}
+              id="next"
+              className="icon"
+              style={{ stroke: "url(#iconGradient)" }}
+            />
+            <ListMusic
+              size={24}
+              strokeWidth={1.5}
+              id="more-music"
+              className="icon"
+              style={{ stroke: "url(#iconGradient)" }}
+            />
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
